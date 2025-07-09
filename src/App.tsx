ae02e59209581665af62eb30ec180cb0e285c901
vkolencik <kolencik@gmail.com>
2025-07-09 17:37:48 +0200

    Use React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
 import { useEffect, useState } from 'react';
 import './App.css'
-import {UserFilter} from './UserFilter'
+import { UserFilter } from './UserFilter'
 import { UserTable } from './UserTable'
 import { searchUsers, type User } from './data'
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [query, setQuery] = useState('');
-  
-  useEffect(() => {
-    const timeoutId = setTimeout(
-      () => {
-        searchUsers(query)
-          .then(results => setUsers(results))
-      },
-      300);
-      
-      return () => clearTimeout(timeoutId);
-  }, [query]);
+  const [search, setSearch] = useState('');
 
-  return (
-    <>
-      <UserFilter onSearchUpdate={setQuery}/>
-      <UserTable users={users}/>
-    </>
+  const query = useQuery({ 
+    queryKey: ['users', search],
+    queryFn: () => searchUsers(search)    
+   });
+
+  return (<>
+    <UserFilter onSearchUpdate={setSearch} />
+    {query.isPending 
+      ? "Loading..." 
+      : <UserTable users={query.data ?? []} />
+    }
+  </>
   )
 }
 
